0139b30ae0b121332718c098d4a1a15e02e0b2d4
liting <luz.liting@gmail.com>
2024-09-16 13:53:33 +0800

    chore: init

---
+++ README.md
@@ -0,0 +1,3 @@
+# uanim
+
+Animation & Transition
---
+++ bump.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'bumpp'
+
+export default defineConfig({
+  preid: 'beta',
+  commit: true,
+  tag: true,
+  push: true,
+  all: true,
+  confirm: true,
+  execute: 'pnpm build',
+  recursive: true,
+})
